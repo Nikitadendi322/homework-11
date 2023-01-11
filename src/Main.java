a67e3bdc171385641d8916db2c2ddd100d7c4db4
Никита <Ployka322@yandex.ru>
2023-01-12 01:19:28 +0700

    homework-11

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
-public class Main {
+class Homework {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printIsLeapYear(2024);
+        recommendApplicationVersion(0, 1990);
+        calculateDeliveryDays()
+    }
+    /*
+     *
+     * @param yearsIsLeap -
+     * @param years -год
+     */
+
+    public static void printIsLeapYearsResult(int year, boolean yearsIsLeap) {
+        if (yearsIsLeap) {
+            System.out.println(year + "високосный");
+        } else {
+            System.out.println(year + " не високосный");
+        }
+    }
+
+    public static void printIsLeapYear(int year) {
+        boolean yearsIsLeap = isLeap(year);
+        printIsLeapYearsResult(year, yearsIsLeap);
+    }
+
+    public static boolean isLeap(int year) {
+        return year % 4 == 0 && year * 100 != 0 || year % 400 == 0;
+    }
+
+    //task2
+    public static void recommendApplicationVersion(int clientIos, int deviceYear) {
+        boolean deviceIsOld = isDeviceOld(deviceYear);
+        System.out.print("Установите ");
+        if (deviceIsOld) {
+            System.out.print("lite");
+        }
+        System.out.print("версию для ");
+        if (clientIos == 0) {
+            System.out.println("IOS");
+        } else {
+            System.out.println("Android");
+        }
+        //task 3
+        public static void calculateDeliveryDays(int deliveryDistance){
+            int deliveryDays =1;
+            if (daliveryDistance>20){
+                deliveryDays++;
+            }
+            if (deliveryDistance)
+
+        }
+
     }
 }
+
